4df15725c643ccdf949b71d0934f1c486611e540
Kwaay <benoit.dum74@gmail.com>
2021-11-04 13:54:34 +0100

    Mise en place du sytème de token sécurisé

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -29,7 +30,11 @@ exports.login = (req,res,next) => {
             }
             res.status(200).json({
                 userId : user._id,
-                token : 'TOKEN'   
+                token: jwt.sign(
+                    {userId : user._id},
+                    'V6RaZg53kb%LrWft47QPXwYu48cy@peeN5DaSMLpgLHq2W*MTCp5m6LB#Y5HrMY7hPm#!C@W&fK@9P88H&vp2$FqcaGsj!&s56eebiNfKE^bpAbCbW*4gzPbU&GLnTBz',
+                    {expiresIn: '24h'}
+                )  
             });
         })
         .catch(error => res.status(500).json({error}))
